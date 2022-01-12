e37158626d5c3cf36b9e44dabc29a0fd73819ad8
Dmitry Mishakov <welzevul0@gmail.com>
2022-01-12 23:44:13 +0300

    Need FIX

--- src/main/java/com/dedazazay/calculator/controllers/GetController.java
+++ src/main/java/com/dedazazay/calculator/controllers/GetController.java
@@ -4,38 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/calculation")
 public class GetController {
 
     @GetMapping("/calculate")
-    public String calculator(@RequestParam("a") int a,
+    public String calculator(/*@RequestParam("a") int a,
                              @RequestParam("b") int b,
-                             @RequestParam("action") String action,
+                             @RequestParam("action") String action,*/
                              Model model) {
-        double result;
-        switch (action) {
-            case("multiplication"):
-                result = a * b;
-                break;
-            case("addition"):
-                result = a + b;
-                break;
-            case("subtraction"):
-                result = a - b;
-                break;
-            case("division"):
-                result = a / b;
-                break;
-            default:
-                result = 0;
-                break;
-        }
-        model.addAttribute("action", action);
-        model.addAttribute("result", result);
+//        double result;
+//        switch (action) {
+//            case("multiplication"):
+//                result = a * b;
+//                break;
+//            case("addition"):
+//                result = a + b;
+//                break;
+//            case("subtraction"):
+//                result = a - b;
+//                break;
+//            case("division"):
+//                result = a / b;
+//                break;
+//            default:
+//                result = 0;
+//                break;
+//        }
+        model.addAttribute("action", "division");
+        model.addAttribute("result", 14D);
 
-        return "calculation/result";
+        return "calculation/calculate";
     }
 }
