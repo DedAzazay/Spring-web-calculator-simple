9336e7c35fd41f2df97905af339ca1d6517af04e
Dmitry Mishakov <welzevul0@gmail.com>
2022-01-12 22:57:44 +0300

    Made controller for calculator

---
+++ src/main/java/com/dedazazay/calculator/controllers/GetController.java
@@ -0,0 +1,39 @@
+package com.dedazazay.calculator.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GetController {
+
+    @GetMapping("/calculate")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+                             Model model) {
+        double result;
+        switch (action) {
+            case("multiplication"):
+                result = a * b;
+                break;
+            case("addition"):
+                result = a + b;
+                break;
+            case("subtraction"):
+                result = a - b;
+                break;
+            case("division"):
+                result = a / b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        model.addAttribute("action", action);
+        model.addAttribute("result", result);
+
+        return "calculation/result";
+    }
+}
